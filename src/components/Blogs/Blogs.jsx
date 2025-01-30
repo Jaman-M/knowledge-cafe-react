59c388b27e8ed6aeef918a884c74db30eeef3ddc
Jaman-M <jaman.manik1@gmail.comm>
2025-01-31 05:31:59 +0800

    create fake data and load data; set layout and make react responsive

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Blogs = () => {
+
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    }, [])
+
+    return (
+        <div className="w-2/3">
+            <h2>This is blogs: {blogs.length}</h2>
+        </div>
+    );
+};
+
+export default Blogs;
