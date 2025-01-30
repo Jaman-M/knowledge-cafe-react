6342bcb6d19fb73e325f9f949b69dafd7d85e1df
Jaman-M <jaman.manik1@gmail.comm>
2025-01-31 06:23:53 +0800

    display blog data and Hashtags

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
 
@@ -14,6 +15,12 @@ const Blogs = () => {
     return (
         <div className="w-2/3">
             <h2>This is blogs: {blogs.length}</h2>
+            {
+                blogs.map(blog => <Blog 
+                    key={blog.id}
+                    blog={blog}
+                    ></Blog>)
+            }
         </div>
     );
 };
